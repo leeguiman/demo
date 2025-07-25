ad014644226121325a87e4fb8b5cb8bf89dc9695
Richard <leeguiman@gmail.com>
2025-07-25 11:51:44 +0800

    Fix Monaco Editor worker loading error

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -11,6 +11,39 @@
 </template>
 
 <script setup lang="ts">
+// Configure Monaco Editor environment for Vite
+self.MonacoEnvironment = {
+  getWorker: function (workerId, label) {
+    const getWorkerModule = (moduleUrl, label) => {
+      return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
+        name: label,
+        type: 'module'
+      });
+    };
+
+    switch (label) {
+      case 'json':
+        return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label);
+      case 'css':
+      case 'scss':
+      case 'less':
+        return getWorkerModule('/monaco-editor/esm/vs/language/css/css.worker?worker', label);
+      case 'html':
+      case 'handlebars':
+      case 'razor':
+        return getWorkerModule('/monaco-editor/esm/vs/language/html/html.worker?worker', label);
+      case 'typescript':
+      case 'javascript':
+        return getWorkerModule('/monaco-editor/esm/vs/language/typescript/ts.worker?worker', label);
+      default:
+        return getWorkerModule('/monaco-editor/esm/vs/editor/editor.worker?worker', label);
+    }
+  },
+  getWorkerUrl: function (moduleUrl) {
+    return new URL(moduleUrl, import.meta.url).href;
+  }
+};
+
 import { ref, onMounted, onUnmounted } from 'vue'
 import * as monaco from 'monaco-editor'
 
